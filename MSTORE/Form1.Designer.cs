92c88c52eecaf26c0aad25160826ba443a4a85ea
Hanif Hamidi <Hanif@Melayu.org.uk>
2019-03-31 20:01:46 +0100

    EMERG MSTORE update

---
+++ MSTORE/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace MSTORE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen1 = new System.Windows.Forms.Button();
+            this.btnOpen2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen1
+            // 
+            this.btnOpen1.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen1.Location = new System.Drawing.Point(12, 30);
+            this.btnOpen1.Name = "btnOpen1";
+            this.btnOpen1.Size = new System.Drawing.Size(225, 34);
+            this.btnOpen1.TabIndex = 0;
+            this.btnOpen1.Text = "MagasinStore";
+            this.btnOpen1.UseVisualStyleBackColor = false;
+            this.btnOpen1.Click += new System.EventHandler(this.BtnOpen1_Click);
+            // 
+            // btnOpen2
+            // 
+            this.btnOpen2.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen2.Location = new System.Drawing.Point(12, 70);
+            this.btnOpen2.Name = "btnOpen2";
+            this.btnOpen2.Size = new System.Drawing.Size(225, 34);
+            this.btnOpen2.TabIndex = 1;
+            this.btnOpen2.Text = "AtauMerci";
+            this.btnOpen2.UseVisualStyleBackColor = false;
+            this.btnOpen2.Click += new System.EventHandler(this.BtnOpen2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(256, 176);
+            this.Controls.Add(this.btnOpen2);
+            this.Controls.Add(this.btnOpen1);
+            this.Name = "Form1";
+            this.Text = "MSTORE";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen1;
+        private System.Windows.Forms.Button btnOpen2;
+    }
+}
+
